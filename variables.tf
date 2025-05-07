270ee67b47bf55b4d3241eccfee96ad1458ccb9c
Jason Schoch <Jason.Schoch@hashicorp.com>
2025-05-06 21:22:52 -0400

    change variables

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "template_org" {
 
 variable "template_repo" {
   description = "The name of the repository in Github that contains the example app code."
-  default     = "terraform-github-static-app"
+  default     = "waypoint-tutorial"
 }
 
 variable "destination_org" {
